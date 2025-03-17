54f901d99829aa3ea99d4fa4ce48267f983526c0
Tomifko <tomas.gonda.3@gmail.com>
2025-03-17 08:01:09 +0100

    add missing params to getAccesToken func

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Get Spotify access token
-async function getAccessToken() {
+async function getAccessToken(clientId, clientSecret) {
     const response = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+            Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
         },
         body: "grant_type=client_credentials",
     });
